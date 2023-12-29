f22b2922a4630fe215445e834dbb81ba5b53c9de
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-12-29 18:58:21 -0300

    First commit

---
+++ src/main/java/fiap/programacao_reativa/finance/FinanceApplication.java
@@ -0,0 +1,13 @@
+package fiap.programacao_reativa.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/programacao_reativa/finance/FinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.programacao_reativa.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
